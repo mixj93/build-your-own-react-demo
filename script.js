6fdb8b0455c160cd680a0c2922310d644103acb8
Li Xueli <mixj93@163.com>
2021-10-09 19:15:12 +0800

    add step 1

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-// const element = <h1 title="foo">Hello</h1>
-
-// const element = React.createElement(
-//   "h1",
-//   { title: "foo" },
-//   "Hello"
+// const element = (
+//   <div id="foo">
+//     <a>bar</a>
+//     <b />
+//   </div>
 // )
+// const container = document.getElementById("root")
+// ReactDOM.render(element, container)
 
-// More: https://github.com/facebook/react/blob/f4cc45ce962adc9f307690e1d5cfa28a288418eb/packages/react/src/ReactElement.js#L111
-const element = {
-  type: "h1",
-  props: {
-    title: "foo",
-    children: "Hello",
-  },
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  }
 }
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+
+const Didact = {
+  createElement,
+}
+
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+)
 
 const container = document.getElementById("root")
 
-// ReactDOM.render(element, container)
-
-const node = document.createElement(element.type)
-node["title"] = element.props.title
-const text = document.createTextNode("")
-text["nodeValue"] = element.props.children
-node.appendChild(text)
-container.appendChild(node)
+ReactDOM.render(element, container)
