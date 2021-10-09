8f0accfc4b36747034adb57486d00f62bb0945a6
Li Xueli <mixj93@163.com>
2021-10-09 19:27:13 +0800

    add step 2

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// const element = (
-//   <div id="foo">
-//     <a>bar</a>
-//     <b />
-//   </div>
-// )
-// const container = document.getElementById("root")
-// ReactDOM.render(element, container)
-
 function createElement(type, props, ...children) {
   return {
     type,
@@ -31,18 +22,40 @@ function createTextElement(text) {
   }
 }
 
+function render(element, container) {
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type)
+
+  const isProperty = key => key !== "children"
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    })
+
+  element.props.children.forEach(child =>
+    render(child, dom)
+  )
+
+  container.appendChild(dom)
+}
+
 const Didact = {
   createElement,
+  render,
 }
 
 /** @jsx Didact.createElement */
 const element = (
   <div id="foo">
     <a>bar</a>
-    <b />
+    <br />
+    <a>foo</a>
   </div>
 )
 
 const container = document.getElementById("root")
 
-ReactDOM.render(element, container)
+Didact.render(element, container)
